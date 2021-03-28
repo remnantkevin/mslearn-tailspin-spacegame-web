30862ca459e6d7bfd2172cf1fcfb27c2558e63f1
Kevin Elliott <skiimilk@gmail.com>
2021-03-28 14:04:55 +0800

    Updated comment

--- Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
+++ Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
@@ -11,7 +11,7 @@ namespace TailSpin.SpaceGame.Web
 {
     public class LocalDocumentDBRepository<T> : IDocumentDBRepository<T> where T : Model
     {
-        // An in-memory list of all items in the collection.
+        // An in-memory list of all items in the collection. I changed this.
         private readonly List<T> _items;
 
         public LocalDocumentDBRepository(string fileName)
